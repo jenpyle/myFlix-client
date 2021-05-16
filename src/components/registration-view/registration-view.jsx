080940503441bd133bdcd60c69d57b7dbcd439ba
jenpyle <jennypyle440@gmail.com>
2021-05-16 09:23:25 -0600

    Finalize registration-view and remove some console.logs

--- src/components/registration-view/registration-view.jsx
+++ src/components/registration-view/registration-view.jsx
@@ -1,10 +1,7 @@
 import React, { useState } from 'react'; //useState is a react hook
 
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
 import { Container, Card, Row, Col, Button, Form } from 'react-bootstrap';
 import axios from 'axios';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 import './registration-view.scss';
 
